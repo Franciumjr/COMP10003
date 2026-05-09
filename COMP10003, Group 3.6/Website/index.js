3db3e0033322b8eacd80fc8840a8a1dea07780da
Franciumjr <sengkuansk123@gmail.com>
2026-05-09 23:03:36 +1000

    Delete website folder

--- COMP10003, Group 3.6/Website/index.js
+++
@@ -1,5 +0,0 @@
-const spreadsheet = "https://sheetdb.io/api/v1/xapcfl7s9o69q";
-fetch (spreadsheet)
-    .then (response => response.json())
-    .then(data => console.log(data.Location))
-    .catch(error => console.log(err));
